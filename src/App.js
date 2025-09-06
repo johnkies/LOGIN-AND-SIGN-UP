4a6acae1ce88288123e6c8f70fac4d1349a94bb0
cheryletol <etolcheryl74@gmail.com>
2025-09-06 21:45:29 +0800

    Add initial project files including package.json, index.html, App.js, and index.js for React application setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>LOGIN-AND-SIGN-UP</h1>
+      {/* Add your login and sign-up components here */}
+    </div>
+  );
+}
+
+export default App;
